1688f55a5020e994c5107379e8e238188bd8026f
shajeer muhammed <shajeermhmmd@gmail.com>
2017-09-23 11:14:04 +0530

    first push

---
+++ application/controllers/api/Users.php
@@ -0,0 +1,35 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . '/libraries/REST_Controller.php';
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+$headers = apache_request_headers();
+class Users extends REST_Controller {
+
+	public function userDetails_get()
+    {
+    	$headerStringValue = $_SERVER['HTTP_TOCKEN'];
+    	echo " User get".$headerStringValue;
+    }
+
+    public function login(){
+    	
+    }
+
+}
+
+?>
